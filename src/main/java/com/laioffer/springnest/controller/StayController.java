8801645a5d570787be74bad5cabc4c8cbc8af6e3
ztrrrrr <84163642+ztrrrrr@users.noreply.github.com>
2023-10-11 13:21:57 -0400

    Comment V

--- src/main/java/com/laioffer/springnest/controller/StayController.java
+++ src/main/java/com/laioffer/springnest/controller/StayController.java
@@ -26,19 +26,21 @@ public class StayController {
         this.reservationService = reservationService;
     }
 
-
+    // Defines an endpoint that handles HTTP GET requests to "/stays".
+    // It returns a list of Stay objects associated with the currently authenticated user.
     @GetMapping(value = "/stays")
     public List<Stay> listStays(Principal principal) {
         return stayService.listByUser(principal.getName());
     }
 
-
+    // Defines an endpoint that handles HTTP GET requests to "/stays/{stayId}".
+    // It returns the details of a specific Stay based on the provided stayId and the authenticated user.
     @GetMapping(value = "/stays/{stayId}")
     public Stay getStay(@PathVariable Long stayId, Principal principal) {
         return stayService.findByIdAndHost(stayId, principal.getName());
     }
 
-
+    // Defines an endpoint that handles HTTP POST requests to "/stays". This is used to add a new Stay entry.
     @PostMapping("/stays")
     public void addStay(
             @RequestParam("name") String name,
@@ -59,13 +61,14 @@ public class StayController {
         stayService.add(stay, images);
     }
 
-
+    // Delete a stay
     @DeleteMapping("/stays/{stayId}")
     public void deleteStay(@PathVariable Long stayId, Principal principal) {
         stayService.delete(stayId, principal.getName());
     }
 
-
+    // Defines an endpoint that handles HTTP GET requests to "/stays/reservations/{stayId}".
+    // It returns a list of Reservation objects associated with a specific Stay.
     @GetMapping(value = "/stays/reservations/{stayId}")
     public List<Reservation> listReservations(@PathVariable Long stayId) {
         return reservationService.listByStay(stayId);
